a63259c62524c1d3777ed547c7520498a63cbe92
fr3ackkastaa <freack110033222@gmail.com>
2025-10-24 11:21:16 +0300

    Update sample_matrix.cpp

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -1,4 +1,4 @@
-﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
 //
 // Copyright (c) Сысоев А.В.
 //
@@ -10,7 +10,7 @@
 
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
+  TMatrix<int> a(5), b(5), c(5);
   int i, j;
 
   setlocale(LC_ALL, "Russian");
